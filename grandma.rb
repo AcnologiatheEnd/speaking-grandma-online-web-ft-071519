f3b43f3ef1cc0d4791489958ea39014cde63e4f7
Winston  Chung <18.chung.winston@ctkprep.org>
2019-07-17 19:58:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
+    binding.pry
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
